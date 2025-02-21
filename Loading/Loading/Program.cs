f8ce3db53112ba8a2ee4be4920417121f054b425
rnjswlals1028 <112091074+rnjswlals1028@users.noreply.github.com>
2025-02-21 15:06:15 +0900

    loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(1000); //1000 1초다 (1000 밀리초)
+            Console.Clear(); // 콘솔 화면 지우기
+        }
+    }
+}
