70aac5e121ee0901583e0dfce531630719284aaa
rnjswlals1028 <112091074+rnjswlals1028@users.noreply.github.com>
2025-02-21 10:42:38 +0900

    study3

---
+++ likelion3/likelion3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace likelion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 리터럴: 코드에서 고정된 값을 의미한다.
+            int num = 10; // 정수형 리터럴
+            double pi = 3.14; // 실수형 리터럴
+            char letter = 'A'; // 문자 리터럴
+            string name = "Alice"; // 문자열 리터럴
+
+            Console.WriteLine(num); // 출력 10
+            Console.WriteLine(pi); // 출력 3.14
+            Console.WriteLine(letter); // 출력 A
+            Console.WriteLine(name); // 출력 Alice
+        }
+    }
+}
---
+++ likelionhw1/likelionhw1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace likelionhw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hp = "hp : 100";
+            string att = "att : 56.7";
+            string name = "캐릭터 이름 : ??";
+            string grade = "등급 : S";
+
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(name);
+            Console.WriteLine(grade);
+
+        }
+    }
+}
