f11956dc1feb911028a9d97454b3876cb164dc58
rnjswlals1028 <112091074+rnjswlals1028@users.noreply.github.com>
2025-02-21 16:57:26 +0900

    study

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        // 로딩바 시작 화면
+        Console.WriteLine("¡¡3초 후, 자¿으로 ¿¿됩니다¡");
+        Thread.Sleep(3000);
+        Console.Clear();
+
+        Console.WriteLine("Enter¡key를 누르면 다음으로 넘어¡가¿");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("666¿notFoundErr?or?(&^&EW#*@¿Ndw231.");
+        Thread.Sleep(1500);
+        Console.Clear();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("   ██   ██   ██   ██   ██   ██    ██   ██    ██   ██    ██   ██ ");
+        Console.WriteLine("    ██ ██     ██ ██     ██ ██      ██ ██      ██ ██      ██ ██");
+        Console.WriteLine("     ███       ███       ███        ███        ███        ███");
+        Console.WriteLine("    ██ ██     ██ ██     ██ ██      ██ ██      ██ ██      ██ ██");
+        Console.WriteLine("   ██   ██   ██   ██   ██   ██    ██   ██    ██   ██    ██   ██");
+        Console.WriteLine("\n나가나가나가나가나가나가나가나가나가나가나가나가나가나가나가나가나가나가당신은초대받은적이없습니다");
+        Thread.Sleep(2000);
+        Console.Clear();
+        Console.WriteLine("친절한진행자의본분을다해야지¿¿");
+        Console.WriteLine("                               ██████                              ");
+        Console.WriteLine("                       ████              ████                          ");
+        Console.WriteLine("                  ████                        ████                          ");
+        Console.WriteLine("               ████                              ████                          ");
+        Console.WriteLine("             ████        ██            ██         ████                          ");
+        Console.WriteLine("            ████      ██  ██        ██  ██         ████                          ");
+        Console.WriteLine("           ████      ██    ██     ██     ██         ████                          ");
+        Console.WriteLine("          ████                                       ████                          ");
+        Console.WriteLine("         ████                                         ████                          ");
+        Console.WriteLine("          ████                                       ████                          ");
+        Console.WriteLine("           ████         ██████████████████          ████                          ");
+        Console.WriteLine("            ████         ██            ██          ████                          ");
+        Console.WriteLine("             ████         ██          ██          ████                          ");
+        Console.WriteLine("              ████         ██        ██          ████                          ");
+        Console.WriteLine("               ████          ████████          ████                          ");
+        Console.WriteLine("                   ████                     ████                          ");
+        Console.WriteLine("                          ████        ████                          ");
+        Thread.Sleep(3000);
+        Console.Clear();
+        Console.ResetColor();
+
+        Console.WriteLine("환영합니다!");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("환영합니다!");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("환영합니다!");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("환영합니다!");
+        Thread.Sleep(1000);
+        Console.Clear();
+        Console.WriteLine("환영합니다!");
+    }
+}
