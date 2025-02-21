6ff9bdf6f680654e952acf4dbc3a344c2926ff6b
rnjswlals1028 <112091074+rnjswlals1028@users.noreply.github.com>
2025-02-21 10:22:53 +0900

    study2

---
+++ likelion2/likelion2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace likelion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age; //정수형 변수
+            age = 25; //변수에 값 저장
+
+            Console.WriteLine(age); // 변수에 저장된 값을 출력 (25)
+
+            // 컨트롤 + s
+        }
+    }
+}
